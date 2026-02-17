0d7c68a6fb37b2323039eef9ccde671555cb7141
juanjosepinedafranco7 <juanjosepinedafranco5@gmail.com>
2026-02-16 22:06:09 -0500

    Add ParesMatriz class to count even numbers

---
+++ ParesMatriz.java
@@ -0,0 +1,19 @@
+public class ParesMatriz {
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+        
+        int contadorPares = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] % 2 == 0) {
+                    contadorPares++;
+                }
+            }
+        }
+        System.out.println("Total números pares: " + contadorPares);
+    }
+}
